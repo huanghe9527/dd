c93bd3c4eb885910b1ab96fc58425512d906c7f1
huanghe9527 <56595233+huanghe9527@users.noreply.github.com>
2025-07-22 19:25:09 +0800

    Update setup-fail2ban.sh

--- setup-fail2ban.sh
+++ setup-fail2ban.sh
@@ -25,6 +25,9 @@ install_fail2ban() {
   # 写入 jail.local 主配置
   cat > "$JAIL_CONF" <<EOF
 [DEFAULT]
+allowipv6 = auto
+
+[DEFAULT]
 bantime = 86400             # 封禁时间：1天
 findtime = 600              # 检测时间窗口：10分钟
 maxretry = 5                # 最大重试次数
